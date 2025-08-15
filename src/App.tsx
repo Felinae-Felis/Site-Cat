5d7ec253f06a716c8d5882861f5bf0868af40438
Elias <echavela@nd.edu>
2025-08-14 17:35:26 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+function App() {
+return (
+  <>
+    <article>
+      	<header>
+        	<a href="/">
+          		<input type="button" value={ "Home " }/>
+        	</a>
+        	<a href="/resume">
+          		<input type="button" value={ "Upload A Cat" }/>
+        	</a>
+			<a href="/Credentials">
+          		<input type="button" value={ "Cat Gallery" }/>
+        	</a>
+      	</header>
+      	<section>
+          	<h1>
+            	Cat Site
+          	</h1>
+          	<hr className="rounded"></hr>
+			<br/>
+			<p>
+      text
+  		</p>
+  			<hr className="rounded"></hr>
+      		<br/>
+			<h4>
+  				Popular Gatos:
+    		</h4>
+			<br/>
+        </section>
+	</article>
+	</>
+)}
+
+export default App
+
